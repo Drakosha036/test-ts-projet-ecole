4ee18fec8e33347bc3225326b3a64d86e10dbd77
Sasha <sashakostina@yahoo.com>
2022-07-25 16:58:45 +0200

    creation des tests pour interns

---
+++ src/intern.ts
@@ -0,0 +1,40 @@
+export class Intern {
+    private id : number;
+    private lastName : string;
+    private firstName : string;
+    private birthDate : string;
+    private phone : string;
+    private email : string;
+
+    public getId() : number {
+        return this.id;
+    }
+
+    public getLastName() : string {
+        return this.lastName;
+    }
+
+    public getFirstName() : string {
+        return this.firstName;
+    }
+
+    public getBirthDate() : string {
+        return this.birthDate;
+    }
+
+    public getPhone() : string {
+        return this.phone;
+    }
+
+    public getEmail() : string {
+        return this.email;
+    }
+
+    public setLastName(lastName : string) : void {
+        this.lastName = lastName.toUpperCase();
+    }
+
+    public setFirstName(firstName : string) : void {
+        this.firstName = firstName.toUpperCase();
+    }
+}
---
+++ test/intern.test.ts
@@ -0,0 +1,32 @@
+import { Intern } from './../src/intern';
+
+describe(`Intern class`, () => {
+
+    const testIntern : Intern = new Intern();
+    const firstLetter: string;
+
+    //tester que objet Intern est instancie
+    test(`Intern should be intantiated`, () => {
+        expect(testIntern).toBeInstanceOf(Intern);
+    })
+
+    test(`Last name should be Aubert`, () => {
+        testIntern.setLastName('Aubert');
+        expect(testIntern.getLastName()).toBe('Aubert');
+    })
+
+    test(`First name should be Jean-Luc`, () => {
+        testIntern.setFirstName('Jean-Luc');
+        expect(testIntern.getFirstName()).toBe('Jean-Luc');
+    })
+
+    test(`First letter of Last name must be capital letter`, () => {
+        //Nom doit etre en majuscule
+        expect(testIntern.getLastName()).toBe('aubert'.toUpperCase());
+    })
+
+    test(`First letter of First name must be capital letter`, () => {
+        //Nom doit etre en majuscule
+        expect(testIntern.getFirstName()).toBe('jean-luc'.toUpperCase());
+    })
+})
