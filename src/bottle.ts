dae1c99dbae51084289bd09b9d7fcd4f07b2656c
Sasha <sashakostina@yahoo.com>
2022-07-25 16:13:52 +0200

    creation des tests

--- src/bottle.ts
+++ src/bottle.ts
@@ -2,20 +2,72 @@
 export class Bottle {
     //attributs communs a toutes les bouteilles
     //contenu
-    contenance: number;
-    packaging: string;
-    closingDevice: string;
-    isOpen: boolean;
+    /**
+     * Contenance must be a value in the folowing:
+     * 0.25 | 0.33 | 0.5 | 0.75 | 1 | 1.5 | 2 | 5
+     */
+    private contenance: number;
+    //constante d'une classe - readonly
+    //creer le tableau de type number et on mets les valeurs dedans 
+    //on peut ecrire Array<number> a la place de number[]
+    private readonly contenances: number[] = [
+        0.25, 0.33, 0.5, 0.75, 1, 1.5, 2, 5
+    ]
+
+    public packaging: string;
+    public closingDevice: string;
+    public isOpen: boolean;
 
 
     //methodes:
-    open() : void {
+    public open() : void {
         //body
         this.isOpen = true;
     }
 
-    close() : void {
+    public close() : void {
         //body
         this.isOpen = false;
     }
+
+    //les accesseurs getter setter
+    public getContenance() : number {
+        return this.contenance;
+    }
+
+    public getContenances() : number[] {
+        return this.contenances;
+    }
+
+    public setContenance(contenance: number) : void {
+        if (contenance >= 0.25 && contenance <= 5) {
+            if (
+                //si l'indexOf est present dans le tableau envoi l'index sinon -1
+                this.contenances.indexOf(contenance) !== -1
+            ) {
+                //affecter la valeur
+                this.contenance = contenance;
+            }
+        }
+    }
+
+
+/*
+    //ca fonctionne , mais si regles de jeu change, c'est pas gerable
+    public setContenance(contenance: number) : void {
+        if (contenance === 0.25 || 
+            contenance === 0.33 || 
+            contenance === 0.5 || 
+            contenance === 0.75 || 
+            contenance === 1 || 
+            contenance === 1.5 || 
+            contenance === 2 || 
+            contenance === 5) {
+
+            this.contenance = contenance;
+        }
+    }
+    */
+
+
 }
--- test/unit/bottle.test.ts
+++ test/unit/bottle.test.ts
@@ -2,6 +2,11 @@ import { Bottle } from './../../src/bottle'; //le . veut dire "d'ou je suis" .. 
 
 //describe - propre pour jest framework ,(entitule, ce qu'il fait)
 describe(`Bottle class`, () => {
+
+    //creer une instance de classe
+    const testBottle: Bottle = new Bottle();
+    
+
     test(
         //bon pratique de mettre `` pour pouvoir faire d'interpolation
         `Bottle should be instanciated`, () => {
@@ -11,5 +16,62 @@ describe(`Bottle class`, () => {
             expect(myBottle).toBeInstanceOf(Bottle);
         }
     )
+
+    test(`Bottle contenance should be 1.5`, () => {
+        const myBottle : Bottle = new Bottle();
+        myBottle.setContenance(1.5);
+        expect(myBottle.getContenance()).toBe(1.5);
+    })
+
+    test(`Contenance must be undefined if value is not between  0.25 and 5`, () => {
+        const myBottle : Bottle= new Bottle();
+        myBottle.setContenance(0.15);
+        expect(myBottle.getContenance()).toBeUndefined();
+
+        myBottle.setContenance(5.1);
+        expect(myBottle.getContenance()).toBeUndefined();
+    })
+
+    test(`Contenances value in the scope are valid`, () => {
+        for (const contenanceValue of testBottle.getContenances()) {
+            testBottle.setContenance(contenanceValue);
+            expect(testBottle.getContenance()).toBe(contenanceValue);
+        }
+    })
+
+
+
+
+    /* ne pas faire :)
+    test(`Contenance must be 0.25 | 0.33 | 0.5 | 0.75 | 1 | 1.5 | 2 | 5`, () => {
+        const myBottle : Bottle = new Bottle();
+        myBottle.setContenance(0.25);
+        expect(myBottle.getContenance()).toBe(0.25);
+
+        myBottle.setContenance(0.33);
+        expect(myBottle.getContenance()).toBe(0.33);
+
+        myBottle.setContenance(0.5);
+        expect(myBottle.getContenance()).toBe(0.5);
+
+        myBottle.setContenance(0.75);
+        expect(myBottle.getContenance()).toBe(0.75);
+
+        myBottle.setContenance(1);
+        expect(myBottle.getContenance()).toBe(1);
+
+        myBottle.setContenance(1.5);
+        expect(myBottle.getContenance()).toBe(1.5);
+
+        myBottle.setContenance(2);
+        expect(myBottle.getContenance()).toBe(2);
+
+        myBottle.setContenance(5);
+        expect(myBottle.getContenance()).toBe(5);
+    })
+    */
+
 })
+
+
 
